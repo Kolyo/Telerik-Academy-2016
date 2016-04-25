dea6f22d651882571b6d9f49d4acc2e6bdc119a6
Kolyo <kreten.fenomen@gmail.com>
2016-04-26 00:40:32 +0300

    Two tasks from the example exam

---
+++ C# Part 1/ExamPreparation/01.Printing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double n = double.Parse(Console.ReadLine());
+        double s = double.Parse(Console.ReadLine());
+        float p = float.Parse(Console.ReadLine());
+
+        Console.WriteLine((((n*s)/500)*p).ToString("F2"));
+
+    }
+}
---
+++ C# Part 1/ExamPreparation/02.TextToNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int m = int.Parse(Console.ReadLine());
+        string text = Console.ReadLine();
+        int result = 0;
+        text = text.ToLower();
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] == '@')
+            {
+                Console.WriteLine(result); return;
+            }
+            else if (char.IsDigit(text[i]))
+            {
+                result *= (int)text[i]-48;
+            }
+            else if (char.IsLetter(text[i]))
+            {
+                result += (int)(char)text[i] - 97;
+            }
+            else
+            {
+                result %= m;
+            }
+
+        }
+  
+        Console.WriteLine(result);
+    }
+}
+
+

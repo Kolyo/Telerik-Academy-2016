11a2e95014867589373fc60fadbe5b38a6b6566e
Kolyo Kolev <klkolev@ifao.net>
2016-04-26 11:16:15 +0300

    Two more tasks added

---
+++ C# Part 1/06.Loops/05.Calculate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        double n = int.Parse(Console.ReadLine());
+        double x = double.Parse(Console.ReadLine());
+        double sum = 1;
+        long fact = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            fact *= i;
+            sum += fact / Math.Pow(x, i);
+        }
+        Console.WriteLine(sum.ToString("F5"));
+    }
+}
---
+++ C# Part 1/06.Loops/06.CalculateAgain/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        ulong n = ulong.Parse(Console.ReadLine());
+        ulong k = ulong.Parse(Console.ReadLine());
+        BigInteger kfact = 1;
+        for (ulong i = 1; i <= n; i++)
+        {
+            if (k < i)
+            {
+                kfact *= i;
+            }
+
+        }
+
+        Console.WriteLine(kfact);
+    }
+}

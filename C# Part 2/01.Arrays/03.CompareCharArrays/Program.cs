a8d9c9a6384bac213323d9201a18cfd213b55d35
Kolyo <kreten.fenomen@gmail.com>
2016-05-17 09:14:47 +0300

    CSS HW1

---
+++ C# Part 2/01.Arrays/03.CompareCharArrays/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        char[] arr1 = Console.ReadLine().ToCharArray();
+        char[] arr2 = Console.ReadLine().ToCharArray();
+        bool firstBigger = arr1.Length > arr2.Length;
+
+        if (arr1.Length != arr2.Length)
+        {
+            if (firstBigger)
+            {
+                Console.WriteLine(">");
+            }
+            else
+            {
+                Console.WriteLine(">");
+            }
+        }
+        else
+        {
+            for (int i = 0; i < arr1.Length - 1; i++)
+            {
+                if (arr1[i] != arr2[i])
+                {
+                    if (arr1[i] > arr2[i])
+                    {
+                        Console.WriteLine(">");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("<");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine("=");
+        }
+    }
+}
---
+++ C# Part 2/01.Arrays/04.MaxSequence/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+        int longestSec = 0 , curSec = 1;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i]-1 == arr[i - 1])
+            {
+                curSec++;
+            }
+            else
+            {
+                if (curSec > longestSec)
+                {
+                    longestSec = curSec;
+                    curSec = 1;
+                }
+            }
+        }
+        Console.WriteLine(longestSec);
+    }
+}
